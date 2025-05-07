469c5ee6e13a0606e526eaaabca6e1c0a2226639
kimhotac <rlaghxkr@Gmail.com>
2025-05-07 22:23:56 +0900

    14940 완료
    BFS 이해도 더 올려야 할듯

--- python/14940.py
+++ python/14940.py
@@ -1,51 +1,33 @@
+from collections import deque
 import sys
-import heapq
 
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
-# n : 세로
-# m : 가로
+jido = [list(map(int, input().split())) for _ in range(n)]
 
-# 2에서 출발해서 각 지점까지의 거리를 모으는것이 편할듯
+dist_map = [[0 if jido[i][j] == 0 else -1 for j in range(m)] for i in range(n)]
 
-# 2위치 찾기
-jido = []
-two = []
-dist_map = []
 for i in range(n):
-    tmp = list(map(int, input().split()))
-    jido.append(tmp)
-    if 2 in tmp:
-        two = [i, tmp.index(2)]
-    dist_map.append([-1 if t==1 else 0 for t in tmp])
+    for j in range(m):
+        if jido[i][j] == 2:
+            start = (i, j)
 
-hq = []
-# (거리, 현재좌표, [지나온 길])
-heapq.heapify(hq)
-heapq.heappush(hq, (0, two, [two]))
+q = deque()
+q.append(start)
+visited = [[False]*m for _ in range(n)]
+visited[start[0]][start[1]] = True
+dist_map[start[0]][start[1]] = 0
 
-while hq:
-    dist, idx, path = heapq.heappop(hq)
+while q:
+    x, y = q.popleft()
+    for dx, dy in ((1,0), (0,1), (-1,0), (0,-1)):
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < m:
+            if not visited[nx][ny] and jido[nx][ny] == 1:
+                visited[nx][ny] = True
+                dist_map[nx][ny] = dist_map[x][y] + 1
+                q.append((nx, ny))
 
-    if dist_map != -1 and dist_map[idx[0]][idx[1]] > dist:
-        continue
-
-    dist_map[idx[0]][idx[1]] = dist
-
-    for midx, move in enumerate(((1,0), (0,1), (-1,0), (0,-1))):
-        move_idx = [idx[0]+move[0], idx[1]+move[1]]
-
-        if dist_map[move_idx[0], move_idx[1]] == 0:
-            continue
-        if move_idx in path:
-            continue
-        if move_idx[0] < 0 or move_idx[1] < 0 or move_idx[0]:
-            continue
-        if midx == 0 and move_idx[0] > n:
-            continue
-        if midx == 1 and move_idx[0] > m:
-            continue
-
-        heapq.heappush(hq, (dist+1, move_idx, path.append(move_idx) ))
-
-print(dist_map)
+for row in dist_map:
+    print(' '.join(map(str, row)))
