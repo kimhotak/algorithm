17197d29c3874081b325fe4ec3cb5951225868f6
kimhotac <rlaghxkr@Gmail.com>
2025-06-02 23:26:01 +0900

    20250602

--- python/27453.py
+++ python/27453.py
@@ -5,30 +5,46 @@ start, end = None, None
 town = [[] for _ in range(n)]
 
 for i in range(n):
-    for j in input().strip():
-        if j.isdigit():
-            town[i].append(int(j))
-        elif j == 'S':
+    for j, item in enumerate(input().strip()):
+        if item.isdigit():
+            town[i].append(int(item))
+        elif item == 'S':
             start = (i,j)
             town[i].append(0)
-        elif j == 'H':
+        elif item == 'H':
             end = (i,j)
             town[i].append(0)
         else:
             town[i].append(-1)
-print(n,m,k)
-print(town)
-print(start,end)
-
-dx = (0, 1, 0, -1)
-dy = (1, 0, -1, 0)
 
 from collections import deque
 Q = deque()
-# 이전 좌표, 
-Q.append(start, 0, 0)
-# 좌표, 
+# 좌표, 이전좌표, 거리
+Q.append((start, start, 0))
+# 좌표, 이전좌표
 visited = set()
-visited.add((start, 0))
+d = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
 while Q:
+    (y, x), (by, bx), dist = Q.popleft()
+    if (y, x) == end:
+        print(dist)
+        break
+
+    for dy, dx in d:
+        ny, nx = y+dy, x+dx
+        if not(0<=ny<n) or not(0<=nx<m):
+            continue
+        elif by == ny and bx == nx:
+            continue
+        elif town[ny][nx] == -1:
+            continue
+        elif town[by][bx] + town[y][x] + town[ny][nx] > k:
+            continue
+        elif ((ny, nx), (y, x)) in visited:
+            continue
+
+        Q.append(((ny, nx), (y, x), dist+1))
+        visited.add(((ny, nx), (y, x)))
+else:
+    print(-1)
