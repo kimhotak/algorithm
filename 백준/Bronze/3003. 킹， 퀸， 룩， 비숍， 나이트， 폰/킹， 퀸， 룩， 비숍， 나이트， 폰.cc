e4a84834b6b702b1746eeb00cbfc1ecbd665bd66
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-06-11 09:00:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int pfect_set[6] = {1, 1, 2, 2, 2, 8};
+	for (int i=0; i<6; i++) {
+		int num;
+		cin >> num;
+		cout << pfect_set[i] - num << ' ';
+	}
+	return 0;
+}
