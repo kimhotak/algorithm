c49897d96445b643c6c9c46273be56610ff5886c
kimhotac <rlaghxkr@Gmail.com>
2025-06-16 06:45:00 +0900

    20250616

--- python/14626.py
+++ python/14626.py
@@ -1,18 +1,5 @@
-isbn = list(input())
-m = int(isbn.pop())
-lost_m = 0
-lost_idx = None
-for i in range(12):
-    if isbn[i] == '*':
-        lost_idx = i
-    else:
-        lost_m += int(isbn[i]) * (3 if i%2 else 1)
-lost_m %= 10
-# lost_idx의 값에 따라 다름
-if lost_idx % 2:
-    # *3한값
-    # lost_m + 3 * X = m
-    print((m - lost_m) // 3)
-else:
-    answer = m - lost_m
-    print(answer + (10 if answer < 0 else 0))
+isbn = [int(i) if i.isdigit() else -1 for i in input()]
+check_sum = isbn.pop()
+
+lost_idx = isbn.index(-1)
+lost_check_sum =
