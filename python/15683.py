c4673b416fa0f4b7d6cecb184bd1de24cc7ac5b2
kimhotac <114737603+kimhotac@users.noreply.github.com>
2025-06-27 15:33:49 +0900

    2차 성공
    set 자료형이 합집합을 만드는게 생각보다 시간복잡도가 높아 직접 리스트를 변경하면서 나아가는 방향으로 선택, 성공

--- python/15683.py
+++ python/15683.py
@@ -1,62 +1,57 @@
 import sys
+import copy
 input = sys.stdin.readline
+
+# 데이터 입력
 n, m = map(int, input().split())
+room = [list(map(int, input().split())) for _ in range(n)]
 
-room = []
+# cctv 위치 파악
 cctv = [] # (y, x, cctv종류)
-wall = 0
-
 for i in range(n):
-    tmp = []
-    for j, v in enumerate(map(int, input().split())):
-        tmp.append(v)
-        if 0 < v < 6:
-            cctv.append([i, j, v])
-        elif v == 6:
-            wall += 1
-    room.append(tmp)
+    for j in range(m):
+        if 0 < room[i][j] < 6:
+            cctv.append((i, j, room[i][j]))
 
+# 하 우 상 좌
+dy = (1,0,-1,0)
+dx = (0,1,0,-1)
 
-# 해당 인덱스의 cctv가 볼수 있는 공간의 좌표 set
-# dir 순서와 같음
-cctv_view = []
-dir = ((1,0),(0,1),(-1,0),(0,-1))
-for y, x, _ in cctv:
-    tmp = []
-    for dy, dx in dir:
-        ny, nx = y, x
-        can_view_dir = set()
-        while (0 <= ny < n) and (0 <= nx < m) and room[ny][nx] != 6:
-            can_view_dir.add((ny, nx))
-            ny += dy
-            nx += dx
-        tmp.append(can_view_dir)
-    cctv_view.append(tmp)
+# cctv가 보는 방향 -1 채우기
+def fill(r, y, x, dir):
+    while (0 <= y < n) and (0 <= x < m) and r[y][x] != 6:
+        if r[y][x] == 0:
+            r[y][x] = -1
+        y += dy[dir]
+        x += dx[dir]
+    return
 
-max_view = -1
-all_cctv_case = (None,
-                    ((0,),(1,),(2,),(3,)), # 1번
-                    ((0, 2),(1, 3)), # 2번
-                    ((0,1),(1,2),(2,3),(3,0)), # 3번
-                    ((0,1,2),(1,2,3),(2,3,0),(3,0,1)), # 4번
-                    ((0,1,2,3),) # 5번
-                )
-stack = [] # cctv 순서, 현재 볼수 있는 set
-stack.append((0, set()))
+# cctv 번호별 방향
+dirs = [
+    [],
+    [[0], [1], [2], [3]],                           # 1번 CCTV
+    [[0, 2], [1, 3]],                               # 2번
+    [[0, 1], [1, 2], [2, 3], [3, 0]],               # 3번
+    [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],   # 4번
+    [[0, 1, 2, 3]],                                 # 5번
+]
+
+# cctv가 보는 방향의 종류로 dfs
+stack  = []
+stack.append((0, room))
+min_blind = float('inf')
 
 while stack:
-    cctv_idx, can_see = stack.pop()
-
-    if cctv_idx >= len(cctv):
-        max_view = max(max_view, len(can_see))
+    idx, current_room = stack.pop()
+    if idx >= len(cctv):
+        min_blind = min(min_blind, sum(r.count(0) for r in current_room))
         continue
+    y, x, cctv_type = cctv[idx]
 
-    cctv_type = cctv[cctv_idx][2]
-    
-    for cctv_case in all_cctv_case[cctv_type]:
-        new_see = can_see.copy()
-        for c in cctv_case:
-            new_see |= cctv_view[cctv_idx][c]
-            stack.append((cctv_idx+1, new_see))
+    for dir in dirs[cctv_type]:
+        new_room = copy.deepcopy(current_room)
+        for d in dir:
+            fill(new_room, y, x, d)
+        stack.append((idx+1, new_room))
 
-print(n * m -max_view -wall)
+print(min_blind)
