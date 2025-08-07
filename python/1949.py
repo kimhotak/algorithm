533b3e80ab10747629b7b4d5bc72ff5ca40c3877
kimhotac <rlaghxkr@Gmail.com>
2025-08-07 22:48:33 +0900

    1949/solve

--- python/1949.py
+++ python/1949.py
@@ -1,16 +1,29 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(100000)  # 재귀 한도 증가
+
 n = int(input())
 people = [-1] + list(map(int, input().split()))
-edge = [tuple(map(int, input().split())) for _ in range(n - 1)]
-# edge가 인접한 마을의 번호로 나온다
-# edge를 dp의 한 축으로 삼으면 될듯
+edge = [[] for _ in range(n + 1)]
 
-dp = [0 for _ in range(n)]
-# edge가 0번 인덱스부터 i개 있을때로 dp
+for _ in range(n-1):
+    v1, v2 = map(int, input().split())
+    edge[v1].append(v2)
+    edge[v2].append(v1)
 
-visited = set()
-# 이미 edge로 연결된 마을
+dp = [[0,0] for _ in range(n + 1)]
+visited = [False] * (n + 1)
 
-print(people)
-print(edge)
+def dfs(node):
+    visited[node] = True
+    dp[node][1] = people[node]  # 현재 노드를 우수마을로 선택
+    
+    for next_node in edge[node]:
+        if visited[next_node]:
+            continue
+        dfs(next_node)
+        dp[node][0] += max(dp[next_node][0], dp[next_node][1])  # 현재 노드를 선택하지 않으면 자식은 선택/비선택 가능
+        dp[node][1] += dp[next_node][0]  # 현재 노드를 선택하면 자식은 선택 불가
+
+dfs(1)
+print(max(dp[1][0], dp[1][1]))
