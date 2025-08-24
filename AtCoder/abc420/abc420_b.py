ef990003a870f315008a19f3547bd85a209026f3
kimhotac <rlaghxkr@Gmail.com>
2025-08-24 21:24:26 +0900

    [AtCoder] Solve abc420_b

---
+++ AtCoder/abc420/abc420_b.py
@@ -0,0 +1,25 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+score = [0] * n
+vote = [list(input().strip()) for _ in range(n)]
+
+
+for i in range(m):
+    v = [vote[j][i] for j in range(n)]
+    cnt = Counter(v)
+
+    minority = '1'
+    if cnt['0'] < n/2:
+        minority = '0'
+
+    for i in range(n):
+        if v[i] == minority:
+            score[i] += 1
+
+high = max(score)
+
+print(*[i+1 for i in range(n) if score[i] == high])
