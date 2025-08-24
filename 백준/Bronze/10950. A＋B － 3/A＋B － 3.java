f68d67d8dd78e87bba2a50526630dc515db43f41
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-08-24 15:01:49 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
