30a5c494c9ecd8f643ddb00d8205aa414b48bc15
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-08-24 18:54:17 +0900

    [Bronze IV] Title: 세 수, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] nums = new int[3];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < 3; i ++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        System.out.print(nums[1]);
+    }
+}
