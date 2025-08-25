e3bddd22871f0f5cdf0e56e568e098567de50999
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-08-25 18:56:04 +0900

    [Bronze II] Title: 손익분기점, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder out = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println((c <= b) ? -1 : (a / (c - b)) + 1);
+    }
+}
