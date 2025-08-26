ac4f0de6838f4378ebb5d0d8aecb29eede87c162
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-08-26 22:03:08 +0900

    [Bronze V] Title: 오늘 날짜, Time: 104 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        System.out.println("2025-08-26");
+    }
+}
