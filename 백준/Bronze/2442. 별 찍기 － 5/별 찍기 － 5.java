63ee97a48a5bad79e044b60936167b7c4735bde0
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-08-26 21:44:33 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            sb.append(" ".repeat(n-i-1)).append("*".repeat(2 * i + 1)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
