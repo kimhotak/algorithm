d350eae11a3fe39c3a52bdb9960eb21c79c336dc
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-08-28 19:39:19 +0900

    [Silver II] Title: DFS와 BFS, Time: 232 ms, Memory: 20552 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= n; i ++) {
+            graph[i].sort((a, b) -> {
+                return b-a;
+            });
+        }
+
+        boolean[] visited = new boolean[n + 1];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(v);
+
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            if (visited[node]) continue;
+            visited[node] = true;
+            sb.append(node).append(' ');
+            for (int next : graph[node]) {
+                stack.push(next);
+            }
+        }
+
+        sb.append('\n');
+        for (int i = 1; i <= n; i ++) {
+            graph[i].sort((a, b) -> {
+                return a-b;
+            });
+        }
+
+        visited = new boolean[n + 1];
+        visited[v] = true;
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(v);
+
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            sb.append(node).append(' ');
+            for (int next : graph[node]) {
+                if (visited[next]) continue;
+                visited[next] = true;
+                queue.offer(next);
+            }
+        }
+
+        System.out.print(sb);
+
+
+
+    }
+}
