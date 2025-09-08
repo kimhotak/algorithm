6a8a3474d11570e664d4b6853104e7dc4dc2d027
kimhotac <114737603+kimhotac@users.noreply.github.com>
2025-09-08 17:48:34 +0900

    11659java진행중

--- workspace/java/Main.java
+++ workspace/java/Main.java
@@ -1,8 +1,32 @@
-// Main.java
-// 여러 환경에서 공통으로 사용하는 Java 대표 파일
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Java!");
-        // 문제별 함수 또는 코드 추가
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int sum = 0;
+            for (int j = s - 1; j < e; j++) {
+                sum += nums[j];
+            }
+            sb.append(sum).append('\n');
+        }
+
+        System.out.print(sb);
     }
 }
