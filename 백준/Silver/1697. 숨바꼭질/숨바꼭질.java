8573b85c7c61f3303bc0f41f046c7c5014ad8165
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-09-11 15:52:42 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 16744 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,33 +3,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        final int MAX = 100001;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int time = 0;
+
         Deque<Integer> q = new ArrayDeque<Integer>();
         q.addLast(n);
-        boolean[] visited = new boolean[200001];
+        int[] visited = new int[MAX];
+        Arrays.fill(visited, -1);
+        visited[n] = 0;
 
-        while (true) {
-            Deque<Integer> tmp = new ArrayDeque<Integer>();
-            while (!q.isEmpty()) {
-                int pos = q.pollFirst();
-                if (pos > 200000 || pos < 0 || visited[pos]) continue;
-                if (pos == k) {
-                    System.out.print(time);
-                    return;
-                }
-                visited[pos] = true;
-                tmp.addLast(pos + 1);
-                tmp.addLast(pos - 1);
-                tmp.addLast(pos * 2);
+        while (!q.isEmpty()) {
+            int cur = q.pollFirst();
+            if (cur == k) break;
+            for (int i = 0; i < 3; i++) {
+                int next;
+                if (i == 0) next = cur - 1;
+                else if (i == 1) next = cur + 1;
+                else next = cur * 2;
+
+                if (0 > next || next >= MAX ||
+                    visited[next] != -1) continue;
+                visited[next] = visited[cur] + 1;
+
+                q.addLast(next);
             }
-            q = tmp;
-            time++;
         }
+        System.out.print(visited[k]);
     }
 }
