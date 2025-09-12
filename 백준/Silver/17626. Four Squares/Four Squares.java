150f375ebf0e9bdfadafd039c18ab287efc4b263
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-09-12 12:31:45 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -8,16 +8,14 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n+1];
         Arrays.fill(dp, 4);
-        for (int i = 1; Math.pow(i, 2) <= n; i++) {
-            dp[(int) Math.pow(i, 2)] = 1;
-        }
+        dp[0] = 0;
+        dp[1] = 1;
 
-        for (int i = 1; i <= n; i++) {
-            if (dp[i] == 1) continue;
-            for (int j = 1; j <= i/2; j++) {
-                if (dp[i] > dp[j] + dp[i-j]) dp[i] = dp[j] + dp[i-j];
-                if (dp[i] <= 2) break;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j*j <= i; j++) {
+                if (dp[i] > dp[i - j*j]) dp[i] = dp[i - j*j];
             }
+            dp[i]++;
         }
         System.out.print(dp[n]);
     }
