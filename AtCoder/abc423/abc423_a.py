84e2f150e2eae89003d047109bab78f98734189c
kimhotac <rlaghxkr@Gmail.com>
2025-09-14 21:03:59 +0900

    [AtCoder] Solve abc423_a

---
+++ AtCoder/abc423/abc423_a.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+x, c = map(int, input().split())
+
+# 1000 당 c 의 수수료 수수료 다 떼고 숫료 못함 1000 엔단위
+
+# 1000 * n + c * n < x
+# (1000 + c) * n < x
+# n < x/(1000 + c)
+
+n = x // (1000 + c)
+print(1000 * n)
