10be8303508ed6609beda5870298706685ff0134
kimhotac <rlaghxkr@Gmail.com>
2025-09-14 10:46:00 +0900

    5430 진행중

--- workspace/java/Main.java
+++ workspace/java/Main.java
@@ -6,28 +6,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        ArrayDeque<Integer> fruit = new ArrayDeque<>();
-        ArrayDeque<Integer> count = new ArrayDeque<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int tmp = Integer.parseInt(st.nextToken());
-            if (!fruit.isEmpty() && fruit.peekLast() == tmp) {
-                count.addLast(count.pollLast()+1);
-            }
-            else {
-                fruit.addLast(tmp);
-                count.addLast(1);
-            }
-        }
-
-        n = fruit.size();
-        int res = 0;
-
-        for (int s = 0; s < n; s++) {
-            for (int e = 0; e < n; e++) {
-
-            }
+        int t = Integer.parseInt(br.readLine());
+        while (0 < t--) {
+            String p = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            String
         }
     }
 }
