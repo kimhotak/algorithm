0233f8b10728467298851cfa892a9e95c40f1a9e
kimhotac <114737603+kimhotac@users.noreply.github.com>
2025-09-17 17:50:44 +0900

    14500 진행중

--- workspace/java/Main.java
+++ workspace/java/Main.java
@@ -6,11 +6,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int t = Integer.parseInt(br.readLine());
-        while (0 < t--) {
-            String p = br.readLine();
-            int n = Integer.parseInt(br.readLine());
-            String
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+        for (int r = 0; r < n; r++){
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < m; c++) {
+                arr[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int res = 0;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                Deque<int[]> dq = new ArrayDeque<>();
+                dq.addLast(new int[]{r,c});
+                for (int i = 0; i < 3; i++) {
+                    Deque<int[]> tdq = new ArrayDeque<>();
+                    while (dq) {
+                        int[] cur =
+                    }
+                }
+            }
         }
     }
 }
