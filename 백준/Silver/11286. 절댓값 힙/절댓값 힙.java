0ab993059058ae02a20bda01e2975862bdf19fe1
김호탁 <114737603+kimhotac@users.noreply.github.com>
2025-12-02 09:07:00 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 24616 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -7,24 +7,17 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        Queue<Integer> pq = new PriorityQueue<Integer>();
-        Queue<Integer> mpq = new PriorityQueue<Integer>();
+        Queue<Integer> pq = new PriorityQueue<Integer>((a,b) -> {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+            if (absA == absB) return Integer.compare(a, b);
+            else return Integer.compare(absA, absB);
+        });
 
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());
-
-            if (x == 0) {
-                if (pq.isEmpty() && mpq.isEmpty()) sb.append(0);
-                else if (pq.isEmpty()) sb.append(-mpq.poll());
-                else if (mpq.isEmpty()) sb.append(pq.poll());
-                else if (pq.peek() < mpq.peek()) sb.append(pq.poll());
-                else sb.append(-mpq.poll());
-                sb.append('\n');
-            }
-            else {
-                if (x < 0) mpq.add(-x);
-                else pq.add(x);
-            }
+            if (x == 0) sb.append(pq.isEmpty() ? 0 : pq.poll()).append('\n');
+            else pq.add(x);
         }
         System.out.print(sb);
     }
