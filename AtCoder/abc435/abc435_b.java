655dcc80945348ff0cf1575895b116aa05ad5a75
kimhotak <rlaghxkr@Gmail.com>
2025-12-06 21:22:28 +0000

    Add abc435 abc435_b

---
+++ AtCoder/abc435/abc435_b.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        for (int l = 0; l < n; l++) {
+            for (int len = 1; l+len < n; len++) {
+                int sum = 0;
+                for (int i = l; i <= l+len; i++) {
+                    sum += arr[i];
+                }
+                cnt++;
+                for (int i = l; i <= l+len; i++) {
+                    if (sum % arr[i] == 0){
+                        cnt--;
+                        break;
+                    }
+                }
+            }
+        }
+
+        sb.append(cnt);
+        System.out.print(sb);
+    }
+}
