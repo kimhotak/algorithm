8eef6b4746e881b7b924ea34e3641b88ce8fd2fb
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-12 16:49:17 +0900

    [Bronze V] Title: 크냐?, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0) break;
+            sb.append(a > b ? "Yes" : "No").append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
