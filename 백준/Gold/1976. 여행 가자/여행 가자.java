c807459900ab5babb18a46dece759771298897d3
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-15 12:38:04 +0900

    [Gold IV] Title: 여행 가자, Time: 152 ms, Memory: 17548 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+
+    public static int find(int x) {
+        if (parent[x] == x) return x;
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        parent[rootX] = rootY;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                if (st.nextToken().equals("1")) {
+                    union(i,j);
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int setNum = Integer.parseInt(st.nextToken()) - 1;
+        String res = "YES";
+        for (int i = 1; i < m; i++) {
+            int tmp = Integer.parseInt(st.nextToken()) - 1;
+            if (find(setNum) != find(tmp)) {
+                res = "NO";
+                break;
+            }
+        }
+        sb.append(res);
+        System.out.print(sb);
+    }
+}
