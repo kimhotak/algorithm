eef90f395fc4f56a42136fa088fcea2c4174955b
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-15 12:41:19 +0900

    [Gold IV] Title: 여행 가자, Time: 140 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -13,6 +13,7 @@ public class Main {
     public static void union(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
+        if (rootX == rootY) return;
         parent[rootX] = rootY;
     }
     
@@ -29,18 +30,18 @@ public class Main {
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                if (st.nextToken().equals("1")) {
+                if (i > j && st.nextToken().equals("1")) {
                     union(i,j);
                 }
             }
         }
 
         st = new StringTokenizer(br.readLine());
-        int setNum = Integer.parseInt(st.nextToken()) - 1;
+        int setNum = find(Integer.parseInt(st.nextToken()) - 1);
         String res = "YES";
         for (int i = 1; i < m; i++) {
             int tmp = Integer.parseInt(st.nextToken()) - 1;
-            if (find(setNum) != find(tmp)) {
+            if (setNum != find(tmp)) {
                 res = "NO";
                 break;
             }
