2e28a9c33c9456f8f968e4a6736b82dbbb814557
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-18 21:18:20 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        sb.append("     /~\\\n");
+        sb.append("    ( oo|\n");
+        sb.append("    _\\=/_\n");
+        sb.append("   /  _  \\\n");
+        sb.append("  //|/.\\|\\\\\n");
+        sb.append(" ||  \\ /  ||\n");
+        sb.append("============\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+
+        System.out.println(sb);
+    }
+}
