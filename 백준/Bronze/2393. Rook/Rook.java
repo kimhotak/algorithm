38419f43dc13d0a233be9507f16754ce47bdc25a
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-18 21:14:42 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        sb.append("  ___  ___  ___\n");
+        sb.append("  | |__| |__| |\n");
+        sb.append("  |           |\n");
+        sb.append("   \\_________/\n");
+        sb.append("    \\_______/\n");
+        sb.append("     |     |\n");
+        sb.append("     |     |\n");
+        sb.append("     |     |\n");
+        sb.append("     |     |\n");
+        sb.append("     |_____|\n");
+        sb.append("  __/       \\__\n");
+        sb.append(" /             \\\n");
+        sb.append("/_______________\\\n");
+
+        System.out.println(sb);
+    }
+}
