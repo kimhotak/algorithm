47c80eb1c8ca2994b2c3fd48819eed2b8d05586c
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-18 21:10:13 +0900

    [Bronze V] Title: 웰컴, Time: 104 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.").append("\n");
+
+        System.out.println(sb);
+    }
+}
