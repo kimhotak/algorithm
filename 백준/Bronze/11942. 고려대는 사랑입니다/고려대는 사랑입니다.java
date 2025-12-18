4ed720ea10cbbd6b2ef193f675b976534b8ab56f
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-18 21:23:38 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        sb.append("고려대학교");
+        
+        System.out.println(sb);
+    }
+}
