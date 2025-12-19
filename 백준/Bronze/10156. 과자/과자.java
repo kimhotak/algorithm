1ff1feb493fe9fc91ec7973c7020d9d6ea511aca
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-19 19:28:03 +0900

    [Bronze IV] Title: 과자, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        sb.append(Math.max(k * n - m, 0));
+        
+        System.out.print(sb);
+    }
+}
