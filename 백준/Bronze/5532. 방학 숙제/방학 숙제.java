33d492e01b6efdabc37e920a717b2b9dfb1b2e9c
김호탁 <114737603+kimhotak@users.noreply.github.com>
2025-12-20 20:44:35 +0900

    [Bronze IV] Title: 방학 숙제, Time: 96 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int l = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+
+        sb.append(l - Math.max((int) Math.ceil((double) a / c), (int) Math.ceil((double) b / d)));
+
+        System.out.print(sb);
+    }
+}
