f48b4764667e97105e2411d9d41879f61208618e
kimhotak <rlaghxkr@Gmail.com>
2025-12-20 21:01:34 +0000

    Add abc437 abc437_a

---
+++ AtCoder/abc437/abc437_a.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        sb.append(a * 12 + b);
+        
+        System.out.print(sb);
+    }
+}
