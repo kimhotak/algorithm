abc8a63ccd72482f11c4d4eaa4ff505f4ce91ce4
kimhotak <rlaghxkr@Gmail.com>
2025-12-27 22:03:04 +0000

    Add abc438 abc438_c

---
+++ AtCoder/abc438/abc438_c.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int[] a = new int[n];
+        int l = 0;
+        for (int i = 0; i < n; i++) {
+            a[l++] = Integer.parseInt(st.nextToken());
+            while (l >= 4 &&
+                    a[l-1] == a[l-2] &&
+                    a[l-2] == a[l-3] &&
+                    a[l-3] == a[l-4] ) {
+                l -= 4;
+            }
+        }
+
+        sb.append(l);
+
+        System.out.print(sb);
+    }
+}
