8703cdf68b92bd641337cefb6ed1c9a8ad8fca27
kimhotac <rlaghxkr@Gmail.com>
2025-12-27 21:24:27 +0900

    [AtCoder] Solve abc438_a

---
+++ AtCoder/abc438/abc438_a.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int d = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        sb.append(7 - (d - f) % 7);
+
+        System.out.print(sb);
+    }
+}
