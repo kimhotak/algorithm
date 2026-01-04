b160e622d122acd0a272c78bbf7061aea59ba61a
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-04 11:39:25 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 136 ms, Memory: 15524 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < 4 * n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5 * n; j++) {
+                sb.append("@");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
