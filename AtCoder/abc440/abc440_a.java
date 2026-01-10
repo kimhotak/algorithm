2a700d5fe1ffcb7cb8e5276a63de95fb05c6124d
kimhotak <rlaghxkr@Gmail.com>
2026-01-10 21:01:05 +0000

    Add abc440 abc440_a

---
+++ AtCoder/abc440/abc440_a.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        sb.append(x << y);
+
+        System.out.print(sb);
+    }
+}
