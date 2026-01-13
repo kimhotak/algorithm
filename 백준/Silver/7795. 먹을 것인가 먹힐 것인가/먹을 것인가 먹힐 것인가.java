603d2fad21758f8eb14eb00fdb5650adc97b5f8b
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-13 11:10:11 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 424 ms, Memory: 37944 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int[] a = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(a);
+
+            int[] b = new int[m];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(b);
+
+            int res = 0;
+            int ai = n - 1, bi = m - 1;
+            while (ai >= 0 && bi >= 0) {
+                while (bi >= 0 && a[ai] <= b[bi]) bi--;
+                res += bi + 1;
+                ai--;
+            }
+
+            sb.append(res).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
