4c022601f3c4134d855f22d7fa044c436beeb4e8
kimhotac <rlaghxkr@Gmail.com>
2026-01-24 20:55:59 +0900

    7490 하던거

--- workspace/java/Main.java
+++ workspace/java/Main.java
@@ -7,34 +7,60 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine());
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            n = Integer.parseInt(br.readLine());
+            res.clear();
+            stack = new int[n];
+            stack[0] = 1;
+            top = 1;
+            dfs(2);
+            Collections.sort(res);
+            for (String s: res) sb.append(s).append('\n');
+            sb.append('\n');
         }
 
-        List<Integer> tail = new ArrayList<>();
-        tail.add(a[0]);
 
-        for (int i = 1; i < n; i++) {
-            int lo = 0, hi = tail.size() - 1;
-            if(a[i] > tail.get(hi)) {
-                tail.add(a[i]);
-                continue;
-            }
-
-            while (lo <= hi) {
-                int mid = (lo + hi) / 2;
-                if (a[i] > tail.get(mid)) lo = mid + 1;
-                else hi = mid - 1;
-            }
-            tail.set(lo, a[i]);
-        }
-
-        sb.append(tail.size());
 
         System.out.print(sb);
+    }
+
+    static int n;
+    static List<String> res = new ArrayList<>();
+    static int[] stack;
+    static int top;
+
+    public static void dfs(int depth) {
+        if (depth > n) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(1);
+            int sum = 0;
+            for (int i = 0; i < top; i++) {
+                sum += stack[i];
+            }
+            if (sum != 0) return;
+
+            for (int i = 0; i < top; i++) {
+                if (stack[i] > 0) sb.append('+');
+                sb.append(i);
+            }
+
+            res.add(sb.toString());
+            return;
+        }
+        // +
+        stack[top++] = depth;
+        dfs(depth + 1);
+        top--;
+
+        // -
+        stack[top++] = -depth;
+        dfs(depth + 1);
+        top--;
+
+        // 합치기
+        stack[top-1] = stack[top] * 10 + depth;
+        dfs(depth + 1);
+
     }
 }
