08aaeb1dc5b4f280e9532b436a04f7945a839c59
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 23:16:26 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.cc
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+#include <utility> // pair가 들어있는 헤더
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cin.ignore();
+    while (n--) {
+        string s;
+        getline(cin, s);
+        s[0] = toupper(s[0]);
+        cout << s << '\n';
+    }
+
+    return 0;
+}
