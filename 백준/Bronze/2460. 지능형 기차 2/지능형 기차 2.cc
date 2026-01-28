eec8c0ea30135b98be25f84121ddfd990a4ad99f
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 21:01:56 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int train = 0, max = 0;
+    for (int i = 0; i < 10; i++) {
+        int out, in;
+        cin >> out >> in;
+		train = train - out + in;
+        if (max < train) max = train;
+    }
+	cout << max << '\n';
+
+    return 0;
+}
