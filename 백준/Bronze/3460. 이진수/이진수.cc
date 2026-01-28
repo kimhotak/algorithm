087fd1bb4a08abfff503f974234f06a5fcfffea7
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 21:43:41 +0900

    [Bronze III] Title: 이진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3460. 이진수/이진수.cc
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 0; i < 32; i++) {
+            if (n & (1 << i)) cout << i << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
