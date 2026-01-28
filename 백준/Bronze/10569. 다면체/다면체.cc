06f93cb1f90e84fa85d91adcb79fe6681a527582
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 20:17:54 +0900

    [Bronze IV] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int v, e;
+        cin >> v >> e;
+        cout << 2 - v + e << '\n';
+    }
+
+    return 0;
+}
