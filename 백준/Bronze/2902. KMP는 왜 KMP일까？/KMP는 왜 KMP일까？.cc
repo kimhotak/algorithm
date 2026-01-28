706de22445e8db13956ccb999d8ec90e0aceabfa
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 22:38:08 +0900

    [Bronze III] Title: KMP는 왜 KMP일까?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+#include <utility> // pair가 들어있는 헤더
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    for (char c : s) {
+        if ('A' <= c && c <= 'Z') cout << c;
+    }
+    return 0;
+}
