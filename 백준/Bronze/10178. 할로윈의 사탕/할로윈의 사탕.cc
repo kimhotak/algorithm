5b8aa477efc7c25eca0df338d89d233a548ef5bf
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 20:14:03 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int c, v;
+        cin >> c >> v;
+        cout << "You get " << c / v <<  " piece(s) and your dad gets " << c % v << " piece(s).\n";
+    }
+
+    return 0;
+}
