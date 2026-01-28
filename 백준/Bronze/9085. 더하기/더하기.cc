d08ca342d32d0157536ab487411d11cf5557ec07
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 20:44:00 +0900

    [Bronze IV] Title: 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int sum = 0;
+        while (n--) {
+            int tmp;
+            cin >> tmp;
+            sum += tmp;
+        }
+        cout << sum << '\n';
+    }
+
+    return 0;
+}
