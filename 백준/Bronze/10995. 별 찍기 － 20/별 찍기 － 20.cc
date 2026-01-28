4e75ff4999cfdfab9e6e34b804b937f594e5baa6
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 20:32:11 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (! (i & 1)) cout << "* ";
+            else cout << " *";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
