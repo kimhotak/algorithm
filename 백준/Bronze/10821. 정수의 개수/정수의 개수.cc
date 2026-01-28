f7fa94f3ad9127e28e0241702bed46e9f67203b6
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-28 23:23:21 +0900

    [Bronze II] Title: 정수의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+#include <utility> // pair가 들어있는 헤더
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for (char c : s) if (c == ',') cnt++;
+    cout << cnt+1;
+    return 0;
+}
