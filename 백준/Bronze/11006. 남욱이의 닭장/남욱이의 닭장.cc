9da70de9c8a50d0da613be4214d728fe31e7a5cd
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-30 12:44:42 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.cc
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+// <bits/stdc++.h> gcc 전용
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm> // sort, max, min 등
+#include <iomanip>   // setw, setfill 등
+#include <climits>   // INT_MAX 등
+#include <utility> // pair가 들어있는 헤더
+#include <math.h>
+#include <numeric> // gcd, lcm이 들어있는 헤더
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int leg, chekin;
+		cin >> leg >> chekin;
+        cout << (chekin << 1) - leg << ' ' << leg - chekin << '\n';
+    }
+
+    return 0;
+}
