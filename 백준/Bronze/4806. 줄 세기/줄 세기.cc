b86a689465ac17b1ad5543f29a7653384654f435
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-30 22:46:17 +0900

    [Bronze III] Title: 줄 세기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+#include <utility>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int cnt = 0;
+    string s;
+    while (getline(cin, s)) cnt++;
+    cout << cnt;
+
+    return 0;
+}
