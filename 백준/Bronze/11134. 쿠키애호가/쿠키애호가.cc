0da0b8587dd2f2e381acd6ff9231f1682967dd7c
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-30 15:09:54 +0900

    [Bronze III] Title: 쿠키애호가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11134. 쿠키애호가/쿠키애호가.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+#include <utility>
+#include <cmath>
+#include <numeric>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, c;
+        cin >> n >> c;
+        cout << n / c + (n % c ? 1 : 0) << '\n';
+    }
+
+    return 0;
+}
