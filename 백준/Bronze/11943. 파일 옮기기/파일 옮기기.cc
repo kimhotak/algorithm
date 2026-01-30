2b5a6b68f69016211f5469a2547a81033c397eb0
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-30 23:09:52 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+#include <utility>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << min(a + d, b + c);
+
+    return 0;
+}
