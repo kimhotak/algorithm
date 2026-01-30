43a662e25b41e79227d94d869f1cc730a8392726
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-30 22:41:55 +0900

    [Bronze III] Title: 아이들은 사탕을 좋아해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9550. 아이들은 사탕을 좋아해/아이들은 사탕을 좋아해.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+#include <utility>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int cnt = 0;
+        while (n--) {
+            int candy;
+            cin >> candy;
+            cnt += candy / k;
+        }
+        cout << cnt << '\n';
+    }
+
+    return 0;
+}
