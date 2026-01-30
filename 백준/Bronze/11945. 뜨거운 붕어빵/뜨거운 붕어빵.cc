5c9484e90cc2e360e6383feb3218b89020599369
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-01-30 23:11:27 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+#include <utility>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    string s;
+    while (cin >> s) {
+        reverse(s.begin(), s.end());
+        cout << s << '\n';
+    }
+
+    return 0;
+}
