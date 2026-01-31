0e210c636429f3dd319120109e801d6ad25c6935
kimhotak <rlaghxkr@Gmail.com>
2026-01-31 21:05:48 +0000

    Add abc443 abc443_a

---
+++ AtCoder/abc443/abc443_a.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    cin >> s;
+    cout << s << 's';
+
+    return 0;
+}
+
