1e2243525953c7f2257a05da09bdf351d575877c
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-02-03 11:02:24 +0900

    [Bronze II] Title: 8진수, 10진수, 16진수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11816. 8진수， 10진수， 16진수/8진수， 10진수， 16진수.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string x;
+    cin >> x;
+    int j = 0;
+	cout << stoi(x, nullptr, 0);
+
+    return 0;
+}
