fa60724d4f75cc9cb89fa973acdd3acc3ab5eb60
김호탁 <114737603+kimhotak@users.noreply.github.com>
2026-02-04 13:16:13 +0900

    [level 2] Title: 미로 탈출, Time: 0.39 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 static int dr[] = {-1, 1, 0, 0}, dc[] = {0,0,-1,1};
 
-int mindist(pair<int,int> from, pair<int,int> to, vector<string> maps) {
+int mindist(pair<int,int> from, pair<int,int> to, vector<string>& maps) {
     deque<pair<int,int>> dq;
     vector<vector<int>> dist(maps.size(), vector<int>(maps[0].size(),-1));
     dq.push_back(from);
@@ -17,7 +17,7 @@ int mindist(pair<int,int> from, pair<int,int> to, vector<string> maps) {
         for (int i = 0; i < 4; i++) {
             pair<int,int> next = {cur.R + dr[i], cur.C + dc[i]};
             if (next.R < 0 || maps.size() <= next.R ||
-                next.C < 0 || maps[next.C].size() <= next.C ||
+                next.C < 0 || maps[next.R].size() <= next.C ||
                 dist[next.R][next.C] != -1 ||
                 maps[next.R][next.C] == 'X') continue;
             dist[next.R][next.C] = dist[cur.R][cur.C] + 1;
@@ -38,8 +38,8 @@ int solution(vector<string> maps) {
             else if (maps[r][c] == 'E') e = {r, c};
         }
     }
-    int s2l = mindist(s,l,maps);
-    int l2e = mindist(l,e,maps);
+    int s2l = mindist(s, l, maps);
+    int l2e = mindist(l, e, maps);
     int answer;
     if (s2l == -1 || l2e == -1) answer = -1;
     else answer = s2l + l2e;
